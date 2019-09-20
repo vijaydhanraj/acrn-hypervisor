4f256b1775d5b251bfa55eb2ed26084b20829a46
Vijay Dhanraj <vijay.dhanraj@intel.com>
2019-09-20 14:29:41 -0700

    acrn-config: Generate target xml file with multiple RDT resources
    This patch adds support for storing multiple RDT resource
    and its max supported clos value in the target xml file under
    clos subsection.
    
    Tracked-On: #3715
    Signed-off-by: Vijay Dhanraj <vijay.dhanraj@intel.com>

--- misc/acrn-config/target/clos.py
+++ misc/acrn-config/target/clos.py
@@ -5,7 +5,7 @@
 
 import parser_lib
 
-CACHE_TYPE = {
+RDT_TYPE = {
         "L2":4,
         "L3":2
     }
@@ -16,8 +16,6 @@ def dump_cpuid_reg(cmd, reg):
     :param cmd: command what can be executed in shell
     :param reg: register name
     """
-    cache_t = ''
-
     res = parser_lib.cmd_execute(cmd)
     if reg == "ebx":
         idx = 3
@@ -37,14 +35,14 @@ def dump_cpuid_reg(cmd, reg):
         reg_value = line.split()[idx].split('=')[1]
 
         if reg == "ebx":
-            if int(reg_value, 16) & CACHE_TYPE['L2'] != 0:
-                cache_t = "L2"
+            cache_t = []
+            if int(reg_value, 16) & RDT_TYPE['L2'] != 0:
+                cache_t.append("L2")
                 break
-            elif int(reg_value, 16) & CACHE_TYPE['L3'] != 0:
-                cache_t = "L3"
-                break
-            else:
-                cache_t = False
+
+            if int(reg_value, 16) & RDT_TYPE['L3'] != 0:
+                cache_t.append("L3")
+
                 break
         elif reg == "edx":
             cache_t = int(reg_value, 16) + 1
@@ -55,33 +53,34 @@ def dump_cpuid_reg(cmd, reg):
 
 def get_clos_info():
     """Get clos max and clos cache type"""
-    clos_max = 0
-    clos_cache = False
+    rdt_clos_max = []
+    rdt_res = []
     cmd = "cpuid -r -l 0x10"
-    clos_cache = dump_cpuid_reg(cmd, "ebx")
+    rdt_res = dump_cpuid_reg(cmd, "ebx")
 
-    if clos_cache == "L2":
-        cmd = "cpuid -r -l 0x10 --subleaf 2"
-    elif clos_cache == "L3":
-        cmd = "cpuid -r -l 0x10 --subleaf 1"
+    if len(rdt_res) == 0:
+        parser_lib.print_yel("Resource Allocation is not supported!")
     else:
-        clos_max = 0
-        parser_lib.print_yel("CLOS is not supported!")
-        return (clos_cache, clos_max)
+        for i in range(len(rdt_res)):
+            if rdt_res[i] == "L2":
+                cmd = "cpuid -r -l 0x10 --subleaf 2"
+                rdt_clos_max.append(dump_cpuid_reg(cmd, "edx"))
 
-    clos_max = dump_cpuid_reg(cmd, "edx")
+            if rdt_res[i] == "L3":
+                cmd = "cpuid -r -l 0x10 --subleaf 1"
+                rdt_clos_max.append(dump_cpuid_reg(cmd, "edx"))
 
-    return (clos_cache, clos_max)
+    return (rdt_res, rdt_clos_max)
 
 
 def generate_info(board_info):
     """Generate clos information
     :param board_info: this is the file which stores the hardware board information
     """
-    (clos_cache, clos_max) = get_clos_info()
+    (rdt_res, rdt_res_clos_max) = get_clos_info()
 
     with open(board_info, 'a+') as config:
         print("\t<CLOS_INFO>", file=config)
-        print("\tclos supported by cache:{}".format(clos_cache), file=config)
-        print("\tclos max:{}".format(clos_max), file=config)
+        print("\trdt resources supported:", ', '.join(rdt_res), file=config)
+        print("\trdt resource clos max:",str(rdt_res_clos_max).strip('[]'), file=config)
         print("\t</CLOS_INFO>\n", file=config)
